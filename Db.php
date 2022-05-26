3388a6a93bf82047ed05b7f50c84a559813f1e8d
sfingers <valorbeam.info@gmail.com>
2022-05-26 20:59:50 +0300

    database initialization

--- Db.php
+++ Db.php
@@ -4,17 +4,36 @@
 
 class Db 
 {
-    protected static $instance;
+    private $_connection;
+    private static $_instance; // single instance
+    private $_host = 'localhost';
+    private $_username = 'root';
+    private $_password = '';
+    private $_database = 'app-stay';
 
-    protected function __construct() {}
-
-    public static function getInstance()
+    private function getInstance()
     {
-        if(empty(self::$instance)){
-            $db_info = array(
-                
-            );
+        if(!self::$_instance){
+            self::$_instance = new self();
         }
+        return self::$_instance;
     }
 
+    public function __construct()
+    {
+        $this->_connection = new mysqli($this->_host, $this->_username, $his->_password, $this->_database);
+        if(mysqli_connect_error())
+        {
+            trigger_error("Failed to connect to the Database ".mysqli_connect_error(), E_USER_ERROR);
+            return;
+        }
+
+    }
+
+    private function __clone(){}
+
+    public function getConnection()
+    {
+        return $this->getInstance();
+    }
 }
